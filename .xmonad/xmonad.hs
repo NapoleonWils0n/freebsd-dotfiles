aed1425ce6f696a1498925605d5502e3c3121dde
Daniel J Wilcox <danieljwilcox@gmail.com>
2020-05-13 02:26:25 +0100

    xm

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -16,10 +16,26 @@ import XMonad.Layout.NoBorders
 myModMask       = mod4Mask  -- Sets modkey to super/windows key
 myTerminal      = "urxvt"      -- Sets default terminal
 myBorderWidth   = 1         -- Sets border width for windows
+myLayout = tiled ||| Mirror tiled ||| noBorders (Full)
+  where
+     -- default tiling algorithm partitions the screen into two panes
+     tiled   = Tall nmaster delta ratio
 
+     -- The default number of windows in the master pane
+     nmaster = 1
+
+     -- Default proportion of screen occupied by master pane
+     ratio   = 1/2
+
+     -- Percent of screen to increment by when resizing panes
+     delta   = 3/100
+
+
+-- main
 main = xmonad $ def
     { borderWidth        = myBorderWidth
     , terminal           = myTerminal
     , modMask            = myModMask
+    , layoutHook         = myLayout
     , normalBorderColor  = "#cccccc"
     , focusedBorderColor = "#ff0000" }
