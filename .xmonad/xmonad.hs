e596d1d1f34996cd59ca13fecb82074da5bce375
Daniel J Wilcox <danieljwilcox@gmail.com>
2020-05-14 21:50:06 +0100

    colour

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -30,13 +30,12 @@ myTerminal      = "urxvt"   -- Sets default terminal
 myBorderWidth   = 2         -- Sets border width for windows
 myNormalBorderColor = "#839496"
 myFocusedBorderColor = "#268BD2"
-myppCurrent = "#c3e88d"
-myppVisible = "#c3e88d"
-myppHidden = "#82AAFF"
-myppHiddenNoWindows = "#F07178"
-myppTitle = "#d0d0d0"
-myppSep = "#666666"
-myppUrgent = "#C45500"
+myppCurrent = "#268BD2"
+myppVisible = "#268BD2"
+myppHidden = "#B58900"
+myppHiddenNoWindows = "#93A1A1"
+myppTitle = "#FDF6E3"
+myppUrgent = "#DC322F"
 windowCount = gets $ Just . show . length . W.integrate' . W.stack . W.workspace . W.current . windowset
 
 -- main
@@ -58,7 +57,7 @@ main = do
                         , ppHidden = xmobarColor myppHidden "" . wrap "+" ""   -- Hidden workspaces in xmobar
                         , ppHiddenNoWindows = xmobarColor  myppHiddenNoWindows ""        -- Hidden workspaces (no windows)
                         , ppTitle = xmobarColor  myppTitle "" . shorten 80     -- Title of active window in xmobar
-                        , ppSep =  "<fc=myppSep> | </fc>"                     -- Separators in xmobar
+                        , ppSep =  "<fc=#586E75> | </fc>"                     -- Separators in xmobar
                         , ppUrgent = xmobarColor  myppUrgent "" . wrap "!" "!"  -- Urgent workspace
                         , ppExtras  = [windowCount]                           -- # of windows current workspace
                         , ppOrder  = \(ws:l:t:ex) -> [ws,l]++ex++[t]
