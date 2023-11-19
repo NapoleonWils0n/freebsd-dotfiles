9232c14a3db38a77b27c98909eedd7edeadc46f1
Daniel J Wilcox <danieljwilcox@gmail.com>
2023-11-19 22:51:54 +0000

    freebsd emacs

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -157,7 +157,7 @@
 (tab-bar-mode 1)
 
 ;; tab bar menu bar button
-(setq tab-bar-menu-bar-button "👹")
+(setq tab-bar-menu-bar-button "👿")
 
 
 ;; ----------------------------------------------------------------------------------
