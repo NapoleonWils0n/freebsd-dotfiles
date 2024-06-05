ad9e7bf17f040307dfc10f76b1d96a346641f355
Daniel J Wilcox <danieljwilcox@gmail.com>
2024-06-05 15:32:54 +0100

    emacs

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -788,7 +788,7 @@ variables for update interval, output format, etc."
   (interactive)
   (let ((yt-url (current-kill 0 t)))
     (async-shell-command (concat
-                   "yt-dlp --skip-download --print \"%(title)s\" " (shell-quote-argument yt-url)))))
+                   "yt-dlp --skip-download --no-playlist --print \"%(title)s\" " (shell-quote-argument yt-url)))))
 
 
 ;; yank the async buffer
