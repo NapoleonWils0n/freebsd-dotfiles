f367d42dbd16b8fc1f167b91145fc225f5eaa999
Daniel J Wilcox <danieljwilcox@gmail.com>
2024-10-02 14:09:38 +0100

    doom modeline ignore firefox

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -159,7 +159,7 @@
 (setq doom-modeline-now-playing-interval 1)
 
 ;; ignored players
-(setq doom-modeline-now-playing-ignored-players '(""))
+(setq doom-modeline-now-playing-ignored-players '("firefox"))
 
 ;; playerctl format
 (setq doom-modeline-now-playing-format "[{{duration(position)}}/{{duration(mpris:length)}}] {{title}}")
