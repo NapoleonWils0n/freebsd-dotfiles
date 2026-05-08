f0e099cd439d85ea1618dce250f6d3f06e9b64d5
Daniel J Wilcox <danieljwilcox@gmail.com>
2026-05-08 22:32:33 +0100

    org mode xdg open

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -789,7 +789,7 @@
           ("\\.mov\\'" . "mpv %s")
           ("\\.pdf\\'" . default)))
         browse-url-browser-function 'browse-url-generic
-        browse-url-generic-program "firefox")
+        browse-url-generic-program "xdg-open")
   :config
   (require 'org-tempo)
   (require 'org-protocol)
